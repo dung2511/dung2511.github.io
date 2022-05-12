df5895336e35195c775c927887e2f1a7a1c3992e
dung0811 <ng.vandung2511@gmail.com>
2022-05-12 18:05:57 +0700

    new

---
+++ bai-tap-re-nhanh-1/js.js
@@ -0,0 +1,20 @@
+// Bài 4:
+function translate(str) {
+    let 
+    switch (str) {
+      case vn:
+        console.log("Xin Chào");
+        break;
+      case fr:
+        console.log("Salut");
+        break;
+      case ge:
+        console.log('Hallo');
+        break;
+      case jp:
+        console.log("Kon'nichiwa");
+        break;
+      case kr:
+        console.log("yeoboseyo");
+  }
+}
