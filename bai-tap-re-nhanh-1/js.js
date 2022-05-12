4017f965b7a2780861612784fc805290cae0f68f
dung0811 <ng.vandung2511@gmail.com>
2022-05-12 20:57:17 +0700

    update

--- bai-tap-re-nhanh-1/js.js
+++ bai-tap-re-nhanh-1/js.js
@@ -1,19 +1,17 @@
 // Bài 4:
 function translate(str) {
-    switch (str) {
-      case 'vn':
-        console.log("Xin Chào");
-        break;
-      case 'fr':
-        console.log("Salut");
-        break;
-      case 'ge':
-        console.log('Hallo');
-        break;
-      case 'jp':
-        console.log("Kon'nichiwa");
-        break;
-      case 'kr':
-        console.log("yeoboseyo");
+  switch (str) {
+    case 'fr':
+      return "Salut";
+    case 'ge':
+      return "Hallo" ;
+    case 'jp':
+      return "Kon'nichiwa";
+    case 'kr':
+      return "yeoboseyo";
+    case 'es':
+      return "Hola";
+    default :
+      return "Xin Chào";
   }
 }
