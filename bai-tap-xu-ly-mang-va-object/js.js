2b4f3e8f6a6e11e26118f49caf8b64bedd7fb8dc
dung0811 <ng.vandung2511@gmail.com>
2022-05-17 21:53:01 +0700

    update

--- bai-tap-xu-ly-mang-va-object/js.js
+++ bai-tap-xu-ly-mang-va-object/js.js
@@ -1,31 +1,15 @@
 // Bài 2:
-      function longestString(arr) {
-        for (let i in arr) {
-            let arr1 = arr[i].length;
-            let arr2 = arr[i+1].length;
-          if (arr1 < arr2) {
-            arr.splice(i, 1);
-          }
-        }
-        return arr;
-      }
-      function longestString(arr) {
-        for (let i in arr) {
-          let array = [];
-          let arrayLength = arr[i].length;
-          if (arr[i].length > arrayLength) {
-            arrayLength = arr[i].length;
-            array.push(arr[i]);
-          }
-        } return array;
-      }
-
-      function longestString(arr){
-        let max = arr[0];
-        for ( let i in arr){
-            if ( max.length < arr[i].length){
-                max = arr[i];
-            }
-        } return max;
+function findLongestString(arr) {
+  let newArr = [],
+  maxLength = 0;
+  arr.forEach((str) => {
+    if (str.length > maxLength) {
+      newArr = [];
+      newArr.push(str);
+      maxLength = str.length;
+    } else if (str.length === maxLength) {
+      newArr.push(str);
     }
-
+  });
+  return newArr;
+}
