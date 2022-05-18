db8058e2547d409eeea3b554afe3d866d3cc64ff
dung0811 <ng.vandung2511@gmail.com>
2022-05-18 23:53:20 +0700

    update

--- bai-tap-xu-ly-mang-va-object/js.js
+++ bai-tap-xu-ly-mang-va-object/js.js
@@ -1,7 +1,7 @@
 // Bài 2:
 function findLongestString(arr) {
   let newArr = [],
-  maxLength = 0;
+    maxLength = 0;
   arr.forEach((str) => {
     if (str.length > maxLength) {
       newArr = [];
@@ -12,4 +12,22 @@ function findLongestString(arr) {
     }
   });
   return newArr;
+}
+function longestString(arr) {
+  let max = [],
+    maxLength = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (maxLength < arr[i].length) {
+      max = [];
+      max.push(arr[i]);
+      maxLength === arr[i].length;
+    }
+    for (let i = 0; i < arr.length; i++) {
+      if (maxLength === arr[i].length) {
+        max.push(arr[i]);
+      }
+    }
+  }
+  return max;
 }
